0ba184ccd014af3fc41db2614a5af9382c6e0643
Krasimir Ronkov <s33592@pjwstk.edu.pl>
2026-03-22 01:32:17 +0100

    User entity created

---
+++ Project/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Entities
+{
+    public enum UserType
+    {
+        Student,
+        Employee
+    }
+    public class User
+    {
+        private static int next_id = 1;
+        public int id { get; }
+        public string fname { get; }
+        public string lname { get; }
+
+        public UserType type { get; }
+
+        public User(string fname, string lname, UserType type)
+        {
+            this.id = next_id++;
+            this.fname = fname;
+            this.lname = lname;
+            this.type = type;
+        }
+    }
+}
