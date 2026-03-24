4ff3519929f5240a1425e545b3fbb0d15753fb5e
Krasimir Ronkov <s33592@pjwstk.edu.pl>
2026-03-24 11:00:44 +0100

    IPenaltyService interface and PenaltyService class implemented

---
+++ Project/Services/PenaltyServices/IPenaltyService.cs
@@ -0,0 +1,11 @@
+﻿using Project.Entities;
+
+namespace Project.Services.PenaltyServices
+{
+    public interface IPenaltyService
+    {
+        public decimal CalculatePenalty(Rental rental);
+        public void ApplyPenalty(Rental rental);
+
+    }
+}
---
+++ Project/Services/PenaltyServices/PenaltyService.cs
@@ -0,0 +1,19 @@
+﻿using Project.Entities;
+
+namespace Project.Services.PenaltyServices
+{
+    public class PenaltyService : IPenaltyService
+    {
+        public readonly decimal dailyPenaltyrate = 5.00m;
+        public decimal CalculatePenalty(Rental rental) { 
+             if(rental.ReturnDate <= rental.DueDate)
+                return 0;
+
+             int daysLate = (rental.ReturnDate.Value - rental.DueDate).Days;
+            return daysLate * dailyPenaltyrate;
+        }
+        public void ApplyPenalty(Rental rental) {
+            rental.Penalty = CalculatePenalty(rental);
+        }
+    }
+}
